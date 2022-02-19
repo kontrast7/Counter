b8b3f569a94bb6fde22c7ac7d5035d08ad7a8b50
kontrast7 <maks.mashko@gmail.com>
2022-02-19 21:28:22 +0300

    Updates

---
+++ precache-manifest.24d1653a155654ecdea180bb02a65cf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16cb574df2fb62761c30ff685ef5f07c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5b0bfa419030af29f7da",
+    "url": "/static/css/main.64136b2f.chunk.css"
+  },
+  {
+    "revision": "eacba69b7788fe19d02a",
+    "url": "/static/js/2.b000a2df.chunk.js"
+  },
+  {
+    "revision": "2d0dc3ea56ea29aa89323d664321ba6d",
+    "url": "/static/js/2.b000a2df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b0bfa419030af29f7da",
+    "url": "/static/js/main.b64ed781.chunk.js"
+  },
+  {
+    "revision": "a09792300aedfe1060d4",
+    "url": "/static/js/runtime-main.d8921639.js"
+  }
+]);
